2096b2d6989789931cac07c161e483acf58b143f
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-13 15:02:24 +0000

    Add files via upload

--- Nextion.cpp
+++ Nextion.cpp
@@ -8,35 +8,42 @@ uint32_t INextion::begin(uint32_t speed = 0) {
   const uint8_t rate[8] = {1, 2, 4, 8, 16, 24, 48, 0};
   uint8_t baud = 0;
 
-  do _serial->begin(rate[baud] * 2400UL);
-  while (!transmit("connect") && (7 > ++baud));
+  do  _serial->begin(rate[baud] * 2400UL);
+  while ((write("connect") == "") && (7 > ++baud));
 
   if (speed) {
-    transmit("baud=" + String(speed));
+    write("baud=" + String(speed));
     _serial->begin(speed);
     return speed;
   }
+
   return (rate[baud] * 2400UL);
 }
 
-bool INextion::response() {
+String INextion::read() {
   uint8_t cycle = NEXTION_SERIAL_CYCLES;
   uint16_t length = 0;
+  String string = "";
 
-  if (_command == NEXTION_CMD_STRING_DATA_ENCLOSED) {
-    do if (_serial->available() && (_string += (char)_serial->read()) && length++) cycle = NEXTION_SERIAL_CYCLES;
-    while (cycle-- && ((length < 3) || ((_string[length - 1] & _string[length - 2] & _string[length - 3]) != 0xFF)));
-    return (cycle && (_string = _string.substring(0, length - 3)));
-  }
+  do if (_serial->available() && (string += char(_serial->read())) && length++) cycle = NEXTION_SERIAL_CYCLES;
+  while (cycle-- && ((length < 4) || (((char)string[length - 1] & (char)string[length - 2] & (char)string[length - 3]) != 0xFF)));
 
-  do if (_serial->available()) _buffer[length++] = _serial->read();
-  while (cycle-- && ((length < 3) || ((_buffer[length - 1] & _buffer[length - 2] & _buffer[length - 3]) != 0xFF)));
+  if (cycle) switch ((uint8_t)string[0]) {
 
-  if (cycle) {
-    if (_command == NEXTION_CMD_NUMERIC_DATA_ENCLOSED) _value = (((uint32_t)_buffer[3] << 24) | ((uint32_t)_buffer[2] << 16) | ((uint32_t)_buffer[1] << 8) | _buffer[0]);
-    return true;
-  }
-  return false;
+      case NEXTION_CMD_STRING_DATA_ENCLOSED:
+        return string.substring(1, length - 3);
+
+      case NEXTION_CMD_NUMERIC_DATA_ENCLOSED:
+        return ((uint32_t)string[4] << 24) | ((uint32_t)string[3] << 16) | ((uint32_t)string[2] << 8) | string[1];
+
+      case NEXTION_CMD_CURRENT_PAGE:
+        return String((uint8_t)string[1]);
+
+      default:
+        return string.substring(0, length - 3);
+    }
+    
+  return "";
 }
 
 bool INextion::wait() {
@@ -45,7 +52,14 @@ bool INextion::wait() {
   return cycle;
 }
 
-uint8_t INextion::transmit(String instruction) {
-  if (_serial->print(instruction + char(0xFF) + char(0xFF) + char(0xFF)) && wait() && (_command = _serial->read()) && response()) return _command;
-  return 0;
+String INextion::wave(uint8_t id, uint8_t channel, uint8_t *data, size_t length) {
+  if (write("addt " + String(id) + "," + String(channel) + "," + String(length)) == String(NEXTION_CMD_TRANSPARENT_DATA_READY)) {
+    for (size_t i = 0; i < length;) _serial->write(data[i++]);
+    if (wait()) return read();
+  }
+}
+
+String INextion::write(String instruction) {
+  if (_serial->print(instruction + char(0xFF) + char(0xFF) + char(0xFF)) && wait()) return read();
+  return "";
 }
--- Nextion.h
+++ Nextion.h
@@ -6,7 +6,6 @@
 
 #include "SoftwareSerial.h"
 
-#define NEXTION_SERIAL_SIZE 9
 #define NEXTION_SERIAL_CYCLES 255
 
 #define  NEXTION_EVENT_RELEASE 0
@@ -26,7 +25,6 @@
 #define NEXTION_CMD_START_MICROSD_UPDATE                        0x89 // LISTEN
 #define NEXTION_CMD_TRANSPARENT_DATA_END                        0xFD // WAVE
 #define NEXTION_CMD_TRANSPARENT_DATA_READY                      0xFE // WAVE
-#define NEXTION_CMD_UNKNOWN_MESSAGE                             0xFF // LISTEN
 
 struct nextionComponent {
   int8_t page, id;
@@ -38,36 +36,25 @@ struct nextionTouch {
 };
 
 class INextion {
-  protected:
-    uint8_t _buffer[NEXTION_SERIAL_SIZE];
-
-    bool _event, _pointer;
-    String _string;
-    uint8_t _command, _page, _id;
-    uint16_t _x, _y;
-    uint32_t _value;
-
-    SoftwareSerial *_serial;
-
-    bool response();
+  private:
     bool wait();
 
-  public:
-    const bool &event = _event, &pointer = _pointer;
-    const String &string = _string;
-    const uint8_t &command = _command, &page = _page, &id = _id;
-    const uint16_t &x = _x, &y = _y;
-    const uint32_t &value = _value;
+  protected:
+    SoftwareSerial *_serial;
 
+  public:
     INextion(uint8_t rx, uint8_t tx);
 
     uint32_t begin(uint32_t speed = 0);
-    uint8_t transmit(String instruction);
+    String write(String instruction);
+    String read();
+    String wave(uint8_t id, uint8_t channel, uint8_t *data, size_t length);
 };
 
 class Nextion: public INextion {
   private:
-    typedef void (*nextionPointer) ();
+    typedef void (*nextionPointer) (nextionTouch *);
+    typedef void (*nextionTarget) (uint16_t *, uint16_t *);
 
     struct nextionCallback {
       nextionCallback *next;
@@ -76,6 +63,8 @@ class Nextion: public INextion {
     };
 
     nextionCallback *_callbacks;
+    nextionTarget _target;
+
     nextionCallback *callback(nextionTouch touch, nextionPointer pointer) {
       nextionCallback *item = new nextionCallback;
       item->next = NULL;
@@ -86,6 +75,14 @@ class Nextion: public INextion {
 
   public:
     Nextion(uint8_t rx, uint8_t tx): INextion(rx, tx) {};
+
+    void attach() {
+      _callbacks = NULL;
+    }
+
+    void attach(nextionComponent component, bool event, nextionPointer pointer) {
+      attach({component.page, component.id, event}, pointer);
+    }
 
     void attach(nextionTouch touch, nextionPointer pointer) {
       if (_callbacks) {
@@ -100,37 +97,32 @@ class Nextion: public INextion {
       } else _callbacks = callback(touch, pointer);
     }
 
-    void attach(nextionComponent component, bool event, nextionPointer pointer) {
-      attach({component.page, component.id, event}, pointer);
+    String circle(uint16_t x, uint16_t y, uint16_t r, uint16_t c) {
+      return write("cir " + String(x) + "," + String(y) + "," + String(r) + "," + String(c));
     }
 
-    uint8_t circle(uint16_t x, uint16_t y, uint16_t r, uint16_t c) {
-      return transmit("cir " + String(x) + "," + String(y) + "," + String(r) + "," + String(c));
+    String clear(uint16_t c = 0xFFFFFF) {
+      return write("cls " + String(c));
     }
 
-    uint8_t clear(uint16_t c = 0xFFFFFF) {
-      return transmit("cls " + String(c));
+    String click(uint8_t id, bool event) {
+      return write("click " + String(id) + "," + String(event));
     }
 
-    uint8_t click(uint8_t id, bool event) {
-      return transmit("click " + String(id) + "," + String(event));
+    String crop(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t resource) {
+      return write("picq " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," + String(resource));
     }
 
-    uint8_t crop(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t resource) {
-      return transmit("picq " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," + String(resource));
+    String crop(uint16_t dx, uint16_t dy, uint16_t w, uint16_t h, uint16_t sx, uint16_t sy, uint8_t resource) {
+      return write("xpic " + String(dx) + "," + String(dy) + "," + String(w) + "," + String(h) + "," + String(sx) + "," + String(sy) + "," + String(resource));
     }
 
-    uint8_t crop(uint16_t dx, uint16_t dy, uint16_t w, uint16_t h, uint16_t sx, uint16_t sy, uint8_t resource) {
-      return transmit("xpic " + String(dx) + "," + String(dy) + "," + String(w) + "," + String(h) + "," + String(sx) + "," + String(sy) + "," + String(resource));
+    void detach() {
+      _callbacks = NULL;
     }
 
-    int16_t current() {
-      if (transmit("sendme") && (command == NEXTION_CMD_CURRENT_PAGE)) return _buffer[0];
-      return -1;
-    }
-
-    uint8_t current(uint8_t page) {
-      return transmit("page " + String(page));
+    void detach(nextionComponent component, bool event) {
+      detach({component.page, component.id, event});
     }
 
     void detach(nextionTouch touch) {
@@ -144,137 +136,134 @@ class Nextion: public INextion {
       }
     }
 
-    void detach(nextionComponent component, bool event) {
-      detach({component.page, component.id, event});
+    String disable(uint8_t id) {
+      return write("tsw " + String(id) + ",0");
     }
 
-    uint8_t disable(uint8_t id) {
-      return transmit("tsw " + String(id) + ",0");
+    String enable(uint8_t id) {
+      return write("tsw " + String(id) + ",1");
     }
 
-    uint8_t enable(uint8_t id) {
-      return transmit("tsw " + String(id) + ",1");
+    String erase(uint8_t id) {
+      return write("cle " + String(id) + ",255");
     }
 
-    uint8_t erase(uint8_t id) {
-      return transmit("cle " + String(id) + ",255");
+    String erase(uint8_t id, uint8_t channel) {
+      return write("cle " + String(id) + "," + String(channel));
     }
 
-    uint8_t erase(uint8_t id, uint8_t channel) {
-      return transmit("cle " + String(id) + "," + String(channel));
+    String fillCircle(uint16_t x, uint16_t y, uint16_t r, uint16_t c) {
+      return write("cirs " + String(x) + "," + String(y) + "," + String(r) + "," + String(c));
     }
 
-    uint8_t fillCircle(uint16_t x, uint16_t y, uint16_t r, uint16_t c) {
-      return transmit("cirs " + String(x) + "," + String(y) + "," + String(r) + "," + String(c));
+    String fillRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t c) {
+      return write("fill " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," + String(c));
     }
 
-    uint8_t fillRectangle(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint16_t c) {
-      return transmit("fill " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," + String(c));
+    String get(String attribute) {
+      return write("get " + attribute);
     }
 
-    uint8_t hide(uint8_t id) {
-      return transmit("vis " + String(id) + ",0");
+    String hide(uint8_t id) {
+      return write("vis " + String(id) + ",0");
     }
 
-    uint8_t line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t c) {
-      return transmit("line " + String(x1) + "," + String(y1) + "," + String(x2) + "," + String(y2) + "," + String(c));
+    String line(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t c) {
+      return write("line " + String(x1) + "," + String(y1) + "," + String(x2) + "," + String(y2) + "," + String(c));
     }
 
     uint8_t listen() {
       if (_serial->available() > 3) {
-        switch (_command = _serial->read()) {
+        String string = read();
+
+        switch ((uint8_t)string[0]) {
 
           case NEXTION_CMD_STARTUP:
-            if (response() && (_buffer[0] == 0x00) && (_buffer[1] == 0x00)) break;
-
-          case NEXTION_CMD_TOUCH_COORDINATE_AWAKE:
-          case NEXTION_CMD_TOUCH_COORDINATE_SLEEP:
-            if (response()) {
-              _x = (((uint16_t)_buffer[0] << 8) | _buffer[1]);
-              _y = (((uint16_t)_buffer[2] << 8) | _buffer[3]);
-              _event = _buffer[4];
-              break;
-            }
+            if ((string[1] == 0x00) && (string[2] == 0x00)) break;
 
           case NEXTION_CMD_TOUCH_EVENT:
-            if (response()) {
-              _page = _buffer[0];
-              _id = _buffer[1];
-              _event = _buffer[2];
-              _pointer = false;
-
-              nextionCallback *item = _callbacks;
-              while (item) {
-                if ((item->touch.page == _page) && (item->touch.id == _id) && (item->touch.event == _event) && (_pointer = (bool)item->pointer)) {
-                  item->pointer();
-                  break;
-                }
-                item = item->next;
+            nextionCallback *item = _callbacks;
+            while (item) {
+              if ((item->touch.page == (uint8_t)string[1]) && (item->touch.id == (uint8_t)string[2]) && (item->touch.event == (uint8_t)string[3]) && (item->pointer)) {
+                nextionTouch touch = item->touch;
+                item->pointer(&touch);
+                break;
               }
-              break;
+              item = item->next;
             }
+            break;
 
-          case NEXTION_CMD_SERIAL_BUFFER_OVERFLOW:
-          case NEXTION_CMD_AUTO_ENTER_SLEEP:
-          case NEXTION_CMD_AUTO_ENTER_WAKEUP:
-          case NEXTION_CMD_READY:
+          case NEXTION_CMD_TOUCH_COORDINATE_AWAKE: 
+          case NEXTION_CMD_TOUCH_COORDINATE_SLEEP:
+            if (_target) {
+              uint16_t x = (uint16_t)string[1] << 8 | (uint8_t)string[2], y = (uint16_t)string[3] << 8 | (uint8_t)string[4];
+              _target(&x, &y);
+            }
+            break;
+
+          case NEXTION_CMD_SERIAL_BUFFER_OVERFLOW: 
+          case NEXTION_CMD_AUTO_ENTER_SLEEP: 
+          case NEXTION_CMD_AUTO_ENTER_WAKEUP: 
+          case NEXTION_CMD_READY: 
           case NEXTION_CMD_START_MICROSD_UPDATE:
-            if (response()) break;
-
-          default: return NEXTION_CMD_UNKNOWN_MESSAGE;
+            break;
         }
-        return _command;
+        return (uint8_t)string[0];
       }
       return 0;
     }
 
-    uint8_t picture(uint16_t x, uint16_t y, uint8_t resource) {
-      return transmit("pic " + String(x) + "," + String(y) + "," + String(resource));
+    String page() {
+      return write("sendme");
     }
 
-    String read(String attribute) {
-      if (transmit("get " + attribute)) return ((_command == NEXTION_CMD_STRING_DATA_ENCLOSED) ? _string : String(_value));
-    }
-    
-    bool reboot() {
-      return transmit("rest");
-    }
-    
-    uint8_t rectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t c) {
-      return transmit("draw " + String(x1) + "," + String(y1) + "," + String(x2) + "," + String(y2) + "," + String(c));
+    String page(uint8_t page) {
+      return write("page " + String(page));
     }
 
-    uint8_t reply(bool state) {
-      return transmit("thup=" + String(state));
+    String picture(uint16_t x, uint16_t y, uint8_t resource) {
+      return write("pic " + String(x) + "," + String(y) + "," + String(resource));
     }
 
-    uint8_t show(uint8_t id) {
-      return transmit("vis " + String(id) + ",1");
+    String reboot() {
+      return write("rest");
     }
 
-    uint8_t sleep() {
-      return transmit("sleep=1");
+    String rectangle(uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t c) {
+      return write("draw " + String(x1) + "," + String(y1) + "," + String(x2) + "," + String(y2) + "," + String(c));
     }
 
-    uint8_t text(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t font, uint16_t foreground, uint16_t background, uint8_t alignX, uint8_t alignY, uint8_t fill, String text) {
-      return transmit("xstr " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," +
-                      String(font) + "," + String(foreground) + "," + String(background) + "," + String(alignX) + "," + String(alignY) + "," + String(fill) + "," + text );
+    String reply(bool state) {
+      return write("thup=" + String(state));
     }
 
-    uint8_t wakeup() {
-      return transmit("sleep=0");
+    String show(uint8_t id) {
+      return write("vis " + String(id) + ",1");
     }
 
-    uint8_t wave(uint8_t id, uint8_t channel, uint8_t data) {
-      return transmit("add " + String(id) + "," + String(channel) + "," + String(data));
+    String sleep() {
+      return write("sleep=1");
     }
 
-    uint8_t wave(uint8_t id, uint8_t channel, uint8_t *data, size_t length) {
-      if (transmit("addt " + String(id) + "," + String(channel) + "," + String(length)) && (_command == NEXTION_CMD_TRANSPARENT_DATA_READY)) {
-        for (size_t i = 0; i < length;) _serial->write(data[i++]);
-        if (wait() && (_command = _serial->read()) && response()) return _command;
-      }
-      return 0;
+    void target(nextionTarget pointer) {
+      _target = pointer;
+    }
+
+    String text(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint8_t font, uint16_t foreground, uint16_t background, uint8_t alignX, uint8_t alignY, uint8_t fill, String text) {
+      return write("xstr " + String(x) + "," + String(y) + "," + String(w) + "," + String(h) + "," +
+                   String(font) + "," + String(foreground) + "," + String(background) + "," + String(alignX) + "," + String(alignY) + "," + String(fill) + "," + text );
+    }
+
+    String wakeup() {
+      return write("sleep=0");
+    }
+
+    String wave(uint8_t id, uint8_t channel, uint8_t data) {
+      return write("add " + String(id) + "," + String(channel) + "," + String(data));
+    }
+
+    String wave(uint8_t id, uint8_t channel, uint8_t *data, size_t length) {
+      return INextion::wave(id, channel, *data, length);
     }
 };
 
