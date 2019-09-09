3bd0cd6678fee4a0abd861474426e567db28c2a6
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-09 17:36:48 +0100

    Update Nextion.cpp

--- Nextion.cpp
+++ Nextion.cpp
@@ -4,12 +4,12 @@ INextion::INextion(uint8_t rx, uint8_t tx) {
   _serial = new SoftwareSerial(rx, tx);
 }
 
-uint32_t INextion::begin(uint32_t speed = 0) {
-  const uint8_t rate[8] = {1, 2, 4, 8, 16, 24, 48, 0};
+uint32_t INextion::begin(uint32_t speed) {
+  const uint8_t rate[8] = {1, 2, 4, 8, 16, 24, 48};
   uint8_t baud = 0;
 
   do _serial->begin(rate[baud] * 2400UL);
-  while ((write("connect") == "") && (7 > ++baud));
+  while (!write("connect") && (7 > ++baud));
 
   if (speed) {
     write("baud=" + String(speed));
@@ -20,47 +20,29 @@ uint32_t INextion::begin(uint32_t speed = 0) {
   return (rate[baud] * 2400UL);
 }
 
-String INextion::read() {
-  uint8_t cycle = NEXTION_SERIAL_CYCLES;
+char* INextion::read() {
+  char data[1024] = {};
   uint16_t length = 0;
-  String string = "";
-
-  do if (_serial->available() && (string += char(_serial->read())) && length++) cycle = NEXTION_SERIAL_CYCLES;
-  while (cycle-- && ((length < 4) || (((char)string[length - 1] & (char)string[length - 2] & (char)string[length - 3]) != 0xFF)));
-
-  if (cycle) switch ((uint8_t)string[0]) {
-
-      case NEXTION_CMD_STRING_DATA_ENCLOSED:
-        return string.substring(1, length - 3);
-
-      case NEXTION_CMD_NUMERIC_DATA_ENCLOSED:
-        return String((uint32_t(string[4]) << 24) + (uint32_t(string[3]) << 16) + (uint32_t(string[2]) << 8) + uint8_t(string[1]));
-
-      case NEXTION_CMD_CURRENT_PAGE:
-        return String((uint8_t)string[1]);
-
-      default:
-        return string;
-    }
-    
-  return "";
+  do while (_serial->available()) data[length++] = _serial->read();
+  while ((data[length - 1] & data[length - 2] & data[length - 3]) != 0xFF);
+  return data;
 }
 
 bool INextion::wait() {
-  uint8_t cycle = NEXTION_SERIAL_CYCLES;
+  uint8_t cycle = 255;
   while (--cycle && (!_serial->available()));
   return cycle;
 }
 
-String INextion::wave(uint8_t id, uint8_t channel, uint8_t *data, size_t length) {
-  if (write("addt " + String(id) + "," + String(channel) + "," + String(length)) == String(NEXTION_CMD_TRANSPARENT_DATA_READY)) {
-    for (size_t i = 0; i < length;) _serial->write(data[i++]);
+char* INextion::wave(uint8_t id, uint8_t channel, uint8_t *buffer, size_t length) {
+  char* data = write("addt " + String(id) + "," + String(channel) + "," + String(length));
+  if (data[0] == NEXTION_CMD_TRANSPARENT_DATA_READY) {
+    for (size_t i = 0; i < length;) _serial->write(buffer[i++]);
     if (wait()) return read();
   }
-  return "";
+  return data;
 }
 
-String INextion::write(String instruction) {
-  if (_serial->print(instruction + char(0xFF) + char(0xFF) + char(0xFF)) && wait()) return read();
-  return "";
+char* INextion::write(String data) {
+  if (_serial->print(String(data + char(0xFF) + char(0xFF) + char(0xFF))) && wait()) return read();
 }
