bb9060a607d9fc2a542f17c6cf84d3f390f1bda3
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-09 20:16:08 +0100

    Update weather_station.ino

--- examples/weather_station/weather_station.ino
+++ examples/weather_station/weather_station.ino
@@ -3,31 +3,27 @@
 #include <SDHT.h>
 
 bool event = 0;
-extern volatile unsigned long timer0_millis;
 String text = "";
-
+uint32_t t = millis();
 DTime dtime;
-Nextion nextion(10, 11);
+Nextion nextion(5, 4);
 SDHT dht;
 
 void setup() {
-  nextion.begin(9600);
-
+  Serial.begin(115200);
+  nextion.begin();
   for (int i = 1; i <= 10; i++) nextion.attach({2, i, NEXTION_EVENT_RELEASE}, numericEvent);
   nextion.attach({2, 11, NEXTION_EVENT_RELEASE}, OKEvent);
   nextion.attach({1, 2, NEXTION_EVENT_RELEASE}, configEvent);
   nextion.attach({1, 1, NEXTION_EVENT_RELEASE}, configEvent);
-
   sendData();
 }
 
 void loop() {
-  if (millis() >= 1000) {
-    noInterrupts();
-    timer0_millis -= 1000;
-    interrupts();
-    dtime.tick();
+  if (t + 1000 < millis()) {
     sendData();
+    dtime.tick();
+    t = millis();
   }
   nextion.listen();
 }
