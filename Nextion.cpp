e8a0cc630488f32a954bdb40ead04a17a2de3c8c
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-09 20:13:38 +0100

    Update Nextion.cpp

--- Nextion.cpp
+++ Nextion.cpp
@@ -1,14 +1,14 @@
 #include "Nextion.h"
-#include "Arduino.h"
 
 INextion::INextion(uint8_t rx, uint8_t tx) {
   _serial = new SoftwareSerial(rx, tx);
 }
 
 uint32_t INextion::begin(uint32_t speed) {
-  const uint8_t rate[8] = {1, 2, 4, 8, 16, 24, 48};
+  const uint8_t rate[7] = {1, 2, 4, 8, 16, 24, 48};
   uint8_t baud = 0;
 
+  _serial->setTimeout(10);
   do _serial->begin(rate[baud] * 2400UL);
   while (!write("connect") && (7 > ++baud));
 
@@ -17,13 +17,11 @@ uint32_t INextion::begin(uint32_t speed) {
     _serial->begin(speed);
     return speed;
   }
-
-  _serial->setTimeout(5);
   return (rate[baud] * 2400UL);
 }
 
 char* INextion::read() {
-  char data[1024] = {};
+  char data[1024];
   _serial->readBytes(data, 1024);
   return data;
 }
@@ -40,7 +38,6 @@ char* INextion::wave(uint8_t id, uint8_t channel, uint8_t *buffer, size_t length
     for (size_t i = 0; i < length;) _serial->write(buffer[i++]);
     if (wait()) return read();
   }
-  return data;
 }
 
 char* INextion::write(String data) {
