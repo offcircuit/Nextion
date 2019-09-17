7a63e2841526634e5c412764e87f58063c03d614
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-09-17 12:21:47 +0100

    Update events.ino

--- examples/weather_station/events.ino
+++ examples/weather_station/events.ino
@@ -1,13 +1,13 @@
-void configEvent(uint8_t, uint8_t id) {
+void configEvent(uint8_t, uint8_t id, bool) {
   if (event = (2 - id)) sendFormat(text = decimate(dtime.day) + decimate(dtime.month) + dtime.year);
   else sendFormat(text = decimate(dtime.hour) + decimate(dtime.minute) + decimate(dtime.second));
 }
 
-void numericEvent(uint8_t, uint8_t id) {
+void numericEvent(uint8_t, uint8_t id, bool) {
   sendFormat(text = String(text + String(id - ((id / 10) * 10))).substring(1));
 }
 
-void OKEvent() {
+void OKEvent(uint8_t, uint8_t id, bool) {
   if (event) dtime.setDate(text.substring(4).toInt(), text.substring(2, 4).toInt(), text.substring(0, 2).toInt());
   else dtime.setTime(text.substring(0, 2).toInt(), text.substring(2, 4).toInt(), text.substring(4).toInt());
   sendData();
